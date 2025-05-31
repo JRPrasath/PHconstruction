59a5ca3f96a9e1129fc26f9d944d7cc602522ac5
JRPrasath <143783062+JRPrasath@users.noreply.github.com>
2025-05-31 13:53:08 +0530

    Split frontend and backend deployments

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = process.env.REACT_APP_API_URL || 'https://phc-backend.cyclic.app';
+const API_URL = process.env.REACT_APP_API_URL || 'https://phc-backend-api.vercel.app';
 
 const api = axios.create({
   baseURL: API_URL,
